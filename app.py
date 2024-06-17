fd0f2fe787b2b9b752fbd4f564db182dc2e6e36e
IvonneAngeles <ivonneah99@gmail.com>
2024-06-17 15:35:29 -0600

    corrección de casilla de verificación

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+
+st.title('Datos de los anuncios de venta de choches')
         
 car_data = pd.read_csv('vehicles_us.csv') #leer los datos
 hist_button = st.button('Construir histograma') #crear un botón
 grap_button = st.button('Construir gráfico de dispersión') #crear boton del gráfico
-build_histogram = st.checkbox('Construir un histograma') #casilla de verificación 
-build_graphic = st.checkbox('Construir un gráfico') #casilla de verificación  
-
-st.header('Datos de los anuncios de venta de choches')
+general_data = st.checkbox('Datos generales') #casilla de verificación 
 
 if hist_button: # al hacer clic en el botón
             # escribir un mensaje
@@ -25,6 +24,5 @@ if grap_button:
 
     st.plotly_chart(fig_2, use_container_width=True)
 
-if build_histogram: # si la casilla de verificación está seleccionada
-    st.write('Comparación entre precio y modelo')
-    st.plotly_chart(fig, use_container_width=True)
+if general_data: # si la casilla de verificación está seleccionada
+    st.table(car_data)
